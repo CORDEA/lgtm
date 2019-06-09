48cf06b5662b533a489c1355789e1178d3b1c208
Yoshihiro Tanaka <contact@cordea.jp>
2019-06-09 21:51:30 +0900

    parse standard options

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+#[macro_use]
+extern crate clap;
+
+use clap::App;
+
 fn main() {
-    println!("Hello, world!");
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from_yaml(yaml).get_matches();
+
+    let source = matches.value_of("source").unwrap();
+    let target = matches.value_of("target").unwrap();
+
+    println!("{}", source);
+    println!("{}", target);
 }
