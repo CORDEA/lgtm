d0f66c28f8ffd07342f0928a7d3ae8c5dad0a519
Yoshihiro Tanaka <contact@cordea.jp>
2019-06-15 15:53:01 +0900

    centering

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,10 @@ extern crate imageproc;
 extern crate rusttype;
 
 use clap::App;
-use image::Rgba;
+use std::iter::Iterator;
+use image::{GenericImageView, Rgba};
 use imageproc::drawing::draw_text_mut;
-use rusttype::{FontCollection, Scale};
+use rusttype::{FontCollection, Scale, Rect, point};
 
 fn main() {
     let yaml = load_yaml!("cli.yml");
@@ -18,11 +19,26 @@ fn main() {
 
     let mut image = image::open(source).unwrap();
 
-    let font = Vec::from(include_bytes!("ttf") as &[u8]);
+    let font = Vec::from(include_bytes!("Roboto-Black.ttf") as &[u8]);
     let font = FontCollection::from_bytes(font).unwrap().into_font().unwrap();
 
-    let scale = Scale {x: 100.0, y: 100.0};
-    draw_text_mut(&mut image, Rgba([0u8, 0u8, 0u8, 1u8]), 0, 0, scale, &font, "");
+    let text = "";
 
+    let (w, h) = image.dimensions();
+    let scale = h as f32 / 3.0;
+
+    let scale = Scale {x: scale, y: scale};
+    let point = point(0.0, font.v_metrics(scale).ascent);
+    let glyphs: Vec<Rect<i32>> = font.layout(text, scale, point)
+        .map(|g| g.pixel_bounding_box().unwrap())
+        .collect();
+
+    let width = glyphs.last().unwrap().max.x - glyphs.first().unwrap().min.x;
+    let height = glyphs.iter().map(|b| b.height()).max().unwrap();
+
+    let x = (w / 2) - (width as u32 / 2);
+    let y = (h / 2) - (height as u32 / 2);
+
+    draw_text_mut(&mut image, Rgba([0u8, 0u8, 0u8, 1u8]), x, y, scale, &font, text);
     let _ = image.save(target).unwrap();
 }
