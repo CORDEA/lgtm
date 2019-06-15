1e80b842db3898328062b27b38f451a041605416
Yoshihiro Tanaka <contact@cordea.jp>
2019-06-15 16:11:48 +0900

    make more readable

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,12 @@ use clap::App;
 use std::iter::Iterator;
 use image::{GenericImageView, Rgba};
 use imageproc::drawing::draw_text_mut;
-use rusttype::{FontCollection, Scale, Rect, point};
+use rusttype::{Font, FontCollection, Scale, Rect, point};
+
+struct Size {
+    width: i32,
+    height: i32,
+}
 
 fn main() {
     let yaml = load_yaml!("cli.yml");
@@ -26,8 +31,17 @@ fn main() {
 
     let (w, h) = image.dimensions();
     let scale = h as f32 / 3.0;
-
     let scale = Scale {x: scale, y: scale};
+
+    let Size {width, height} = get_text_size(&font, scale, text);
+    let x = (w / 2) - (width as u32 / 2);
+    let y = (h / 2) - (height as u32 / 2);
+
+    draw_text_mut(&mut image, Rgba([0u8, 0u8, 0u8, 1u8]), x, y, scale, &font, text);
+    let _ = image.save(target).unwrap();
+}
+
+fn get_text_size(font: &Font, scale: Scale, text: &str) -> Size {
     let point = point(0.0, font.v_metrics(scale).ascent);
     let glyphs: Vec<Rect<i32>> = font.layout(text, scale, point)
         .map(|g| g.pixel_bounding_box().unwrap())
@@ -35,10 +49,5 @@ fn main() {
 
     let width = glyphs.last().unwrap().max.x - glyphs.first().unwrap().min.x;
     let height = glyphs.iter().map(|b| b.height()).max().unwrap();
-
-    let x = (w / 2) - (width as u32 / 2);
-    let y = (h / 2) - (height as u32 / 2);
-
-    draw_text_mut(&mut image, Rgba([0u8, 0u8, 0u8, 1u8]), x, y, scale, &font, text);
-    let _ = image.save(target).unwrap();
+    return Size {width: width, height: height};
 }
