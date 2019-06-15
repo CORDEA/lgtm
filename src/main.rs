04fd67c9de29e2af5b215e86ec3f828b9f9a1e31
Yoshihiro Tanaka <contact@cordea.jp>
2019-06-15 17:01:12 +0900

    draw description

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ extern crate rusttype;
 
 use clap::App;
 use std::iter::Iterator;
-use image::{GenericImageView, Rgba};
+use image::{DynamicImage, GenericImageView, Rgba};
 use imageproc::drawing::draw_text_mut;
 use rusttype::{Font, FontCollection, Scale, Rect, point};
 
@@ -14,8 +14,8 @@ const TITLE: &str = "LGTM";
 const DESCRIPTION: &str = "Looks Good To Me";
 
 struct Size {
-    width: i32,
-    height: i32,
+    width: u32,
+    height: u32,
 }
 
 fn main() {
@@ -31,15 +31,32 @@ fn main() {
     let font = FontCollection::from_bytes(font).unwrap().into_font().unwrap();
 
     let (w, h) = image.dimensions();
-    let scale = h as f32 / 3.0;
-    let scale = Scale {x: scale, y: scale};
 
-    let Size {width, height} = get_text_size(&font, scale, TITLE);
-    let x = (w / 2) - (width as u32 / 2);
-    let y = (h / 2) - (height as u32 / 2);
+    let title_scale = h as f32 / 3.0;
+    let title_scale = Scale {x: title_scale, y: title_scale};
+    let title_size = get_text_size(&font, title_scale, TITLE);
 
-    draw_text_mut(&mut image, Rgba([0u8, 0u8, 0u8, 1u8]), x, y, scale, &font, TITLE);
+    let title_x = (w / 2) - (title_size.width / 2);
+    let title_y = (h / 2) - (title_size.height / 2);
+    let desc_y = title_y  + title_size.height + 20;
+
+    let color = Rgba([0u8, 0u8, 0u8, 1u8]);
+    draw_description(&mut image, color, desc_y, &font);
+    draw_text_mut(&mut image, color, title_x, title_y, title_scale, &font, TITLE);
+
     let _ = image.save(target).unwrap();
+}
+
+fn draw_description(image: &mut DynamicImage, color: Rgba<u8>, y: u32 , font: &Font) {
+    let (w, h) = image.dimensions();
+    let desc_scale = h as f32 / 10.0;
+
+    let desc_scale = Scale {x: desc_scale, y: desc_scale};
+    let desc_size = get_text_size(&font, desc_scale, DESCRIPTION);
+
+    let desc_x = (w / 2) - (desc_size.width / 2);
+
+    draw_text_mut(image, color, desc_x, y, desc_scale, &font, DESCRIPTION);
 }
 
 fn get_text_size(font: &Font, scale: Scale, text: &str) -> Size {
@@ -50,5 +67,5 @@ fn get_text_size(font: &Font, scale: Scale, text: &str) -> Size {
 
     let width = glyphs.last().unwrap().max.x - glyphs.first().unwrap().min.x;
     let height = glyphs.iter().map(|b| b.height()).max().unwrap();
-    return Size {width: width, height: height};
+    return Size {width: width as u32, height: height as u32};
 }
